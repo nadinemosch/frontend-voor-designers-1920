8f51f630399ca76d2b1dddaa2868fc7c5e4a4b31
nadinemosch <57796374+nadinemosch@users.noreply.github.com>
2020-05-07 15:18:53 +0200

    Update script.js

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -4,3 +4,12 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', filmToevoegen);
+
+function filmToevoegen(button){
+	var button = document.querySelector('button');
+	button.style.backgroundColor = "green";
+}
