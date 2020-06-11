20022dd7767f0311ebf2d58e094ca7c25374bb5d
nadinemosch <57796374+nadinemosch@users.noreply.github.com>
2020-06-11 13:53:00 +0200

    11 juni aanpassing 1

--- opdracht3/demo/js.js
+++ opdracht3/demo/js.js
@@ -1,11 +1,11 @@
 // variabelen met de html elementen uit de pagina
 // const is een constante variabele, dus ongeveer hetzelfde als var
-
 const header = document.querySelector('header'); // variabele aangemaakt voor de header
 const section = document.querySelector('section'); // variabele aangemaakt voor de section
 
 // variabele met de url waar de JSON vandaan komt
-let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
+let requestURL = 'https://dog.ceo/api/breed/beagle/images/random/3';
+
 // een xml http request variabele aanmaken
 let request = new XMLHttpRequest();
 
@@ -18,39 +18,25 @@ request.send();
 
 // onload/onerror zijn eventlistener van het xmlhttprequest > als de data is geladen voert ie deze functie
 request.onload = function () {
-    const movies = request.response;
+    const doggos = request.response;
     //functie aanroepen en json data aan meegeven als parameter
-    // console.log(movies);
-    showMovies(movies);
+    // console.log(doggos);
+    showDoggos(doggos);
 }
 
-function showMovies(jsonObj) {
-    // console.log("showHeroes", jsonObj["members"]);
-    // var heroes = jsonObj['members'];
-    // console.log("function Showmovies", jsonObj);
-
-    //console.log(jsonObj[0].title);
+function showDoggos(jsonObj) {
+    // console.log("showDoggos", jsonObj["message"]);
+    // console.log("function showDoggos", jsonObj);
+    // console.log(jsonObj[0].message);
 
     for (let i = 0; i < jsonObj.length; i++) {
         //html elementen aanmaken
         const myArticle = document.createElement('article');
-
-        const myH2 = document.createElement('h2');
-        myH2.textContent = jsonObj[i].title;
-
-        const plot = document.createElement('p');
-        plot.textContent = jsonObj[i].plot;
-        
-        const plaatje = document.createElement('img');
-        plaatje.src = jsonObj[i].cover;
-
-        // json data koppelen aan html elementen
-        //myH2.textContent = heroes[i].name;
+        const afbeelding = document.createElement('img');
+        plaatje.src = jsonObj[i].message;
 
         // html elementen genest
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(plot);
-        myArticle.appendChild(plaatje);
+        myArticle.appendChild(afbeelding);
 
         // nieuwe html elementen aan de section in de html
         section.appendChild(myArticle);
